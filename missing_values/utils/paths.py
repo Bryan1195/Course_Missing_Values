38253cdcc116fc87b65a9425497b6f22bbc1b8ee
Bryan Carvajal <brayan.carvajal@utp.ac.pa>
2023-01-29 22:03:11 -0500

    Creating nullity_scatterplot function in missing method

--- missing_values/utils/paths.py
+++ missing_values/utils/paths.py
@@ -1,4 +1,6 @@
 from pyprojroot import here
+import pandas as pd
+import numpy as np
 from pathlib import Path
 from typing import (
     Union,
@@ -48,3 +50,29 @@ for dir_type in [
     ]:
     dir_var = '_'.join(dir_type) + "_dir"
     exec(f"{dir_var} = make_dir_function({dir_type})")
+
+def column_fill_with_dummies(
+    column: pd.Series,
+    proportion_below: float=0.10,
+    jitter: float=0.075,
+    seed: int=42
+) -> pd.Series:
+
+    column = column.copy(deep=True)
+
+    # Extract values metadata
+    missing_mask = column.isna()
+    number_missing_values = missing_mask.sum()
+    column_range = column.max() - column.min()
+
+    # Shift data
+    column_shift = column.min() - column.min() * proportion_below
+
+    # Create the "jitter" (noise) to be added around the points
+    np.random.seed(seed)
+    column_jitter = (np.random.rand(number_missing_values) - 2) * column_range * jitter
+
+    # Save new dummy data
+    column[missing_mask] = column_shift + column_jitter
+
+    return column    
